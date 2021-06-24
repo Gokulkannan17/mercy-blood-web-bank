22b3b1665bb5185e4c1b1672f71c4932f70563da
Gokulkannan17 <78197624+Gokulkannan17@users.noreply.github.com>
2021-06-24 08:04:50 +0530

    homepage

---
+++ home2.php
@@ -0,0 +1,78 @@
+
+<html>
+    <title>Mercy Blood Web Bank</title>
+    
+    
+    <head>
+        <style>
+            body{
+                background-image: url('back3.jpg');
+                background-size:cover;
+                background-repeat: no-repeat;
+                background-color: rgb(26, 219, 210);
+               
+            }
+            h1{
+                text-align:center;
+                font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+                font-weight: bold;
+                margin-top: 80px;
+                /*margin-left:;*/
+            }
+            ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  border: 1px solid #e7e7e7;
+  background-color: #057514;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: inline-block;
+  color: rgb(78, 3, 3);
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  background-color:rgb(18, 116, 5) ;
+}
+li a:hover{
+    border:1px solid;
+    background-color: lightseagreen;
+}
+.Goal{
+    color: rgb(34, 6, 112);
+    font-size: 35px;
+    background-color: lightcoral;
+    display: block;
+}
+h1{
+    text-align: center;
+    margin-left: 90px;
+}
+
+
+        </style>
+        <script type="text/javascript">
+       
+        </script>
+    </head>
+    <body>
+        <h1>BLOOD WEB BANK</h1>
+        <form>
+            <nav>
+                <label class="Goal">MERCY</label>
+                <ul>
+                    <li class="parts"><a href="registerform.php" >Register</a></li>
+                    <li class="parts"><a href="availability.php">Donaters Details</a></li>
+                </ul>
+                
+            </nav>
+        </form>
+    </body>
+
+
